e385dc8b6748d85f3d12721466a7980d1f3a97f5
liaihong <>
2022-08-11 16:30:03 +0800

    add hive api

---
+++ src/HiveData.ts
@@ -0,0 +1,13 @@
+import BigNumber from 'bignumber.js'
+
+const TAG: string = 'HiveData'
+
+export class HiveData {
+  static CommonStatus = {
+    available: 0,
+    deleted: 1,
+    edited: 2,
+    sending: 11,
+    error: 12,
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+'use strict'
+// import { SHA256 } from "./utils/sha256"
+export type {
+}
+
+export {
+
+}
