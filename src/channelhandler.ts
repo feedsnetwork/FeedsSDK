2360848278f602ac43d3f6cc491c1501664669ec
stiartsly <stiartsly@gmail.com>
2022-08-31 21:42:20 +0800

    Improve channelhandler interface

--- src/channelhandler.ts
+++ src/channelhandler.ts
@@ -1,13 +1,26 @@
+import { ChannelInfo } from "./ChannelInfo"
 import { Dispatcher } from "./Dispatcher"
 import { PostChunk } from "./PostChunk"
 
 export interface ChannelHandler {
     /**
+     * Query the channel infomation from remote channel
+     * @returns An promise object that contains channel information.
+     */
+    queryChannelInfo(): Promise<ChannelInfo>;
+
+    /**
+     * Query the channel infomation from remote channel
+     * @returns An promise object that contains channel information.
+     */
+    queryAndDispatchInfo(dispatcher: Dispatcher<ChannelInfo>);
+
+    /**
      *
      * @param earlierThan
      * @param maximum
      */
-     queryPosts(earlierThan: number, maximum: number): Promise<PostChunk[]>
+     queryPosts(earlierThan: number, maximum: number): Promise<PostChunk[]>;
 
     /**
      * Query the list of posts from this channel and dispatch them one by one to
@@ -17,8 +30,7 @@ export interface ChannelHandler {
      * @param upperNumber The maximum number of posts in this query request.
      * @param dispatcher The dispatcher routine to deal with a post.
      */
-     queryAndDispatchPosts(until: number, upperLimit: number, dispatcher: Dispatcher<PostChunk>)
-
+     queryAndDispatchPosts(until: number, upperLimit: number, dispatcher: Dispatcher<PostChunk>);
 
     /**
      * Query the list of Posts from this channel by a speific range of time.
@@ -27,7 +39,7 @@ export interface ChannelHandler {
      * @param end The end timestamp
      * @returns An promise object that contains a list of posts.
      */
-    queryPostsByRangeOfTime(start: number, end: number): Promise<PostChunk[]>
+    queryPostsByRangeOfTime(start: number, end: number): Promise<PostChunk[]>;
 
     /**
      * Query the list of posts from this channel and dispatch them one by one to
@@ -38,7 +50,7 @@ export interface ChannelHandler {
      * @param upperLimit The maximum number of this query
      * @param dispatcher The dispatcher routine to deal with a post
      */
-    queryAndDispatchPostsRangeOfTime(start: number, end: number, upperLimit: number, dispatcher: Dispatcher<PostChunk>)
+    queryAndDispatchPostsRangeOfTime(start: number, end: number, upperLimit: number, dispatcher: Dispatcher<PostChunk>);
 
     /**
      * Query a post by post identifier.
@@ -46,7 +58,7 @@ export interface ChannelHandler {
      * @param postId The post id
      * @returns An promise object that contains the post.
      */
-    queryPost(postId: string): Promise<PostChunk>
+    queryPost(postId: string): Promise<PostChunk>;
 
     /**
      * Query a post and dispatch it to the routine.
@@ -54,13 +66,13 @@ export interface ChannelHandler {
      * @param postId The post id
      * @param dispatcher The routine to deal with the queried post
      */
-    queryAndDispatchPost(postId: string, dispatcher: Dispatcher<PostChunk>)
+    queryAndDispatchPost(postId: string, dispatcher: Dispatcher<PostChunk>);
 
     /**
      * Query the subscriber count of this channel.
      * @returns An promise object that contains the number of subscribers to this channel.
      */
-    querySubscriberCount(): Promise<number>
+    querySubscriberCount(): Promise<number>;
 
     /**
      * Query the list of subscribers to this channel.
