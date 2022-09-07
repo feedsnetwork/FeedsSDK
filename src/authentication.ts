34ed086dcba8c2ce91d43100be5cd4af9cd467aa
liaihong <>
2022-09-08 00:50:23 +0800

    add auth

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,7 +1,7 @@
 import { MyProfile } from "./MyProfile"
-import { connectivity, DID } from '@elastosfoundation/elastos-connectivity-sdk-js'
+import { connectivity, DID as ConDID } from '@elastosfoundation/elastos-connectivity-sdk-js'
 // import { EssentialsConnector } from '@elastosfoundation/essentials-connector-client-browser';
-import { DefaultDIDAdapter, DIDBackend, VerifiablePresentation, DIDDocument } from '@elastosfoundation/did-js-sdk';
+import { DefaultDIDAdapter, DIDBackend, VerifiablePresentation, DIDDocument, JWTParserBuilder, JWTHeader } from '@elastosfoundation/did-js-sdk';
 import { Executable, InsertOptions, File as HiveFile, ScriptRunner, Vault, AppContext, Logger as HiveLogger, UpdateResult, UpdateOptions, Condition, InsertResult } from "@elastosfoundation/hive-js-sdk";
 import { AppContext as FeedsAppContext } from "./appcontext"
 import { Logger } from './utils/logger'
@@ -22,7 +22,6 @@ export class Authentication {
     createHiveAppcontext(): Promise<AppContext> {
         return new Promise(async (resolve, reject) => {
             try {
-                const appInstanceDid = this.feedsAppcontext.getAppDid()
                 const userDid = this.feedsAppcontext.getUserDid()
                 const applicationDid = this.feedsAppcontext.getApplicationDid()
 
@@ -74,10 +73,80 @@ export class Authentication {
         })
     }
 
-    generateHiveAuthPresentationJWT() {
-        // TODO:
+    async generateHiveAuthPresentationJWT(challeng) {
+        if (challeng === null || challeng === undefined || challeng === '') {
+            logger.log('Params error: challeng 不能为空')
+        }
+
+        // Parse, but verify on chain that this JWT is valid first 
+        const JWTParser = new JWTParserBuilder().build()
+        const parseResult = await JWTParser.parse(challeng)
+        const claims = parseResult.getBody()
+        if (claims === undefined) {
+            return // TODO: 抛出error
+        }
+        const payload = claims.getJWTPayload()
+        const nonce = payload['nonce'] as string
+        const hiveDid = claims.getIssuer()
+        const appIdCredential = await this.issueDiplomaFor()
+        const presentation = await this.createPresentation(appIdCredential, hiveDid, nonce)
+        const token = await this.createChallengeResponse(presentation, hiveDid, storePassword)
+        return token
     }
 
+    async issueDiplomaFor() {
+        const applicationDid = this.feedsAppcontext.getApplicationDid()
+        connectivity.setApplicationDID(applicationDid)
+        const didAccess = new ConDID.DIDAccess()
+        let credential = await didAccess.getExistingAppIdentityCredential()
+        if (credential) {
+            return credential
+        }
+
+        credential = await didAccess.generateAppIdCredential()
+
+        if (credential) {
+            return credential
+        }
+    }
+
+    async createPresentation(vc, hiveDid, nonce) {
+        const access = new ConDID.DIDAccess()
+        const info = await access.getOrCreateAppInstanceDID()
+        const info2 = await access.getExistingAppInstanceDIDInfo()
+        const vpb = await VerifiablePresentation.createFor(info.did, null, info.didStore)
+        const vp = await vpb.credentials(vc).realm(hiveDid).nonce(nonce).seal(info2.storePassword)
+        return vp
+    }
+
+    async createChallengeResponse(vp, hiveDid, storepass) {
+        const exp = new Date()
+        const iat = new Date().getTime()
+        exp.setFullYear(exp.getFullYear() + 2)
+        const expTime = exp.getTime()
+
+        // Create JWT token with presentation.
+        const doc = await this.getAppInstanceDIDDoc()
+        const info = await new ConDID.DIDAccess().getExistingAppInstanceDIDInfo()
+        const token = await doc.jwtBuilder()
+            .addHeader(JWTHeader.TYPE, JWTHeader.JWT_TYPE)
+            .addHeader("version", "1.0")
+            .setSubject("DIDAuthResponse")
+            .setAudience(hiveDid)
+            .setIssuedAt(iat)
+            .setExpiration(expTime)
+            .claimsWithJson("presentation", vp.toString(true))
+            .sign(info.storePassword);
+        return token
+    }
+
+    async getAppInstanceDIDDoc() {
+        const didAccess = new ConDID.DIDAccess()
+        const info = await didAccess.getOrCreateAppInstanceDID()
+        const instanceDIDDocument = await info.didStore.loadDid(info.did.toString())
+        logger.log("instanceDIDDocument ======= ", instanceDIDDocument)
+        return instanceDIDDocument
+    }
     /*
     public async signin() {
         // await this.initConnectivitySDK();
