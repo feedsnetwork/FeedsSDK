eed7c3ba41c5c31d46b8e34ad75b4f01db57a464
stiartsly <stiartsly@gmail.com>
2022-09-08 22:54:46 +0800

    WIP: udpate

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 'use strict'
 
 import { AppContext } from "./appcontext"
-import { Channel } from "./channel"
 import { ChannelEntry } from "./channelentry"
-import { ChannelHandler } from "./channelhandler"
-import { ChannelInfo } from "./channelinfo"
 import { Dispatcher } from "./dispatcher"
-import * as Utils from "./utils/utils"
 import { MyChannel } from "./mychannel"
 import { MyProfile } from "./myprofile"
-import { PostA } from "./post"
 import { PostBody } from "./postbody"
 import { Profile } from "./profile"
-import { ProfileHandler } from "./profilehandler"
 import { Logger } from "./utils/logger"
 
 import {
