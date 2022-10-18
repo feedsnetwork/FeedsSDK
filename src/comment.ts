fed529cf0a4925119d9ddfbed2863aa118d544f0
liaihong <>
2022-10-18 15:05:52 +0800

    improve await/async with Comment class

--- src/comment.ts
+++ src/comment.ts
@@ -47,116 +47,114 @@ export class Comment {
  * add comment
  * @param content： comment content
  */
-    public addComment(content: string): Promise<Comment> {
-        const userDid = this.context.getUserDid()
-        const channelId = this.getCommentInfo().getChannelId()
-        const postId = this.getCommentInfo().getPostId()
-        const refcommentId = this.getCommentInfo().getCommentId()
-        const commentId = this.generateCommentId(userDid, postId, refcommentId, content)
-        const createdAt = (new Date()).getTime()
-        let params = {
-            "comment_id": commentId,
-            "channel_id": channelId,
-            "post_id": postId,
-            "refcomment_id": refcommentId,
-            "content": content,
-            "created_at": createdAt,
+    public async addComment(content: string) {
+        try {
+            const userDid = this.context.getUserDid()
+            const channelId = this.getCommentInfo().getChannelId()
+            const postId = this.getCommentInfo().getPostId()
+            const refcommentId = this.getCommentInfo().getCommentId()
+            const commentId = this.generateCommentId(userDid, postId, refcommentId, content)
+            const createdAt = (new Date()).getTime()
+            let params = {
+                "comment_id": commentId,
+                "channel_id": channelId,
+                "post_id": postId,
+                "refcomment_id": refcommentId,
+                "content": content,
+                "created_at": createdAt,
+            }
+            logger.debug("add comment params: ", params)
+    
+            const result = await this.vault.callScript(scripts.SCRIPT_CREATE_COMMENT, params,
+                this.getCommentInfo().getTargetDidDid(), this.context.getAppDid())
+            logger.debug("add comment success: ", result)
+            params["updated_at"] = createdAt
+            params["status"] = 0
+            params["creater_did"] = this.context.getUserDid()
+            const comment = Comment.parse(this.targetDid, params)
+            return comment
+        } catch (error) {
+            logger.error("Add comment error : ", error)
+            throw new Error(error)
         }
-        logger.debug("add comment params: ", params)
-
-        return this.vault.callScript(scripts.SCRIPT_CREATE_COMMENT, params,
-            this.getCommentInfo().getTargetDidDid(), this.context.getAppDid()).then(result => {
-                logger.debug("add comment success: ", result)
-                params["updated_at"] = createdAt
-                params["status"] = 0
-                params["creater_did"] = this.context.getUserDid()
-                const comment = Comment.parse(this.targetDid, params)
-                return comment
-            })
-            .catch(error => {
-                logger.error("Add comment error : ", error)
-                throw new Error(error)
-            })
     }
 /**
  * update comment
  * @param content：comment content
  */
-    public updateComment(content: string): Promise<boolean> {
-        const updatedAt = (new Date()).getTime()
-        const channelId = this.getCommentInfo().getChannelId()
-        const postId = this.getCommentInfo().getPostId()
-
-        const params = {
-            "channel_id": channelId,
-            "post_id": postId,
-            "comment_id": this.getCommentInfo().getCommentId(),
-            "content": content,
-            "updated_at": updatedAt
+    public async updateComment(content: string) {
+        try {
+            const updatedAt = (new Date()).getTime()
+            const channelId = this.getCommentInfo().getChannelId()
+            const postId = this.getCommentInfo().getPostId()
+    
+            const params = {
+                "channel_id": channelId,
+                "post_id": postId,
+                "comment_id": this.getCommentInfo().getCommentId(),
+                "content": content,
+                "updated_at": updatedAt
+            }
+            logger.debug("update comment params: ", params)
+            const result = await this.vault.callScript(scripts.SCRIPT_UPDATE_COMMENT, params,
+                this.getCommentInfo().getTargetDidDid(), this.context.getAppDid())
+            logger.debug("update comment success: ", result)
+            return true
+        } catch (error) {
+            logger.error("Update comment error : ", error)
+            throw new Error(error)
         }
-        logger.debug("update comment params: ", params)
-        return this.vault.callScript(scripts.SCRIPT_UPDATE_COMMENT, params,
-            this.getCommentInfo().getTargetDidDid(), this.context.getAppDid()).then(result => {
-                logger.debug("update comment success: ", result)
-                return true
-            })
-            .catch(error => {
-                logger.error("Update comment error : ", error)
-                throw new Error(error)
-            })
     }
 /**
  * deleteComment
  */
-    public deleteComment() {
-        const params = {
-            "channel_id": this.getCommentInfo().getChannelId(),
-            "post_id": this.getCommentInfo().getPostId(),
-            "comment_id": this.getCommentInfo().getCommentId()
+    public async deleteComment() {
+        try {
+            const params = {
+                "channel_id": this.getCommentInfo().getChannelId(),
+                "post_id": this.getCommentInfo().getPostId(),
+                "comment_id": this.getCommentInfo().getCommentId()
+            }
+            logger.debug("delete comment params: ", params)
+            const targetDid = this.getCommentInfo().getTargetDidDid()
+    
+            const result = await this.vault.callScript(scripts.SCRIPT_DELETE_COMMENT, params,
+                targetDid, this.context.getAppDid())
+            logger.debug("delete comment success: ", result)
+            return true
+        } catch (error) {
+            logger.error("delete comment error : ", error)
+            throw new Error(error)
         }
-        logger.debug("delete comment params: ", params)
-        const targetDid = this.getCommentInfo().getTargetDidDid()
-
-        return this.vault.callScript(scripts.SCRIPT_DELETE_COMMENT, params,
-            targetDid, this.context.getAppDid())
-            .then(result => {
-                logger.debug("delete comment success: ", result)
-                return true
-            })
-            .catch(error => {
-                logger.error("delete comment error : ", error)
-                throw new Error(error)
-            })
     }
 
 /**
  * Query the comments of the specified comment id
  */
-    public queryCommentById(): Promise<Comment> {
-        const params = {
-            "channel_id": this.getCommentInfo().getChannelId(),
-            "post_id": this.getCommentInfo().getPostId(),
-            "comment_id": this.getCommentInfo().getCommentId()
+    public async queryCommentById() {
+        try {
+            const params = {
+                "channel_id": this.getCommentInfo().getChannelId(),
+                "post_id": this.getCommentInfo().getPostId(),
+                "comment_id": this.getCommentInfo().getCommentId()
+            }
+            logger.debug("query comment byId params: ", params)
+            const results = await this.vault.callScript(scripts.SCRIPT_QUERY_COMMENT_BY_COMMENTID, params,
+                this.getCommentInfo().getTargetDidDid(), this.context.getAppDid())
+            logger.debug("query comment byId success: ", results)
+            const result = results.find_message.items
+
+            let comments = []
+            result.forEach(item => {
+                const comment = Comment.parse(this.targetDid, item)
+                comments.push(comment)
+            })
+            logger.debug("query comment by id 'comments': ", comments)
+            return comments[0]
+        } catch (error) {
+            logger.error('query comment by id error:', error)
+            throw new Error(error)
         }
-        logger.debug("query comment byId params: ", params)
-        return this.vault.callScript(scripts.SCRIPT_QUERY_COMMENT_BY_COMMENTID, params,
-            this.getCommentInfo().getTargetDidDid(), this.context.getAppDid()).then(result => {
-                logger.debug("query comment byId success: ", result)
-                return result.find_message.items
-            })
-            .then(result => {
-                let comments = []
-                result.forEach(item => {
-                    const comment = Comment.parse(this.targetDid, item)
-                    comments.push(comment)
-                })
-                logger.debug("query comment by id 'comments': ", comments)
-                return comments[0]
-            })
-            .catch(error => {
-                logger.error('query comment by id error:', error)
-                throw new Error(error);
-            })
     }
 
     public static parse(targetDid: string, data: any): Comment {
