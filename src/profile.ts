1c427a2f53a77a7bd33b07726123bb355cc25c71
stiartsly <stiartsly@gmail.com>
2022-10-19 12:03:52 +0800

    update profile.ts

--- src/profile.ts
+++ src/profile.ts
@@ -1,5 +1,4 @@
 import { RuntimeContext } from "./runtimecontext";
-import { Channel } from "./channel";
 import { ChannelInfo } from "./channelinfo";
 import { ProfileHandler } from "./profilehandler";
 import { hiveService as VaultService } from "./hiveService"
@@ -34,7 +33,7 @@ export class Profile implements ProfileHandler {
         return this.userDid
     }
 
-    // Get owner of this profile 
+    // Get owner of this profile
     public getTargetDid(): string {
         return this.targetDid
     }
@@ -45,15 +44,21 @@ export class Profile implements ProfileHandler {
     }
 
     /**
-    * 新增 1 已讨论 // find_message.total
     * Query the number of all channels created by this profile
     */
     public async queryOwnedChannelCount(): Promise<number> {
         try {
-            const filter = {}
-            const result = await this.vault.callScript(scripts.SCRIPT_PRIFILE_CHANNELS, filter, this.targetDid, this.context.getAppDid())
-            logger.debug("query owned channel count success: ", result)
-            return result.find_message.total
+            const result = await this.vault.callScript(
+                scripts.SCRIPT_PRIFILE_CHANNELS,
+                {},
+                this.targetDid,
+                this.context.getAppDid()
+            )
+            logger.debug(`Call script to query owned channel acount: ${result}`)
+
+            let count = result.find_message.total
+            logger.debug(`Got owned channel count: ${count}`)
+            return count
         } catch (error) {
             logger.error('query owned channels count error: ', error)
             throw new Error(error)
@@ -61,21 +66,25 @@ export class Profile implements ProfileHandler {
     }
 
     /**
-    * 新增 已讨论 // 1
     * Query all channels created by this profile
     */
     public async queryOwnedChannels(): Promise<ChannelInfo[]> {
         try {
-            const filter = {}
-            const results = await this.vault.callScript(scripts.SCRIPT_PRIFILE_CHANNELS, filter, this.targetDid, this.context.getAppDid())
-            logger.debug("query owned channels success: ", results)
-            const result = results.find_message.items
+            let result = await this.vault.callScript(
+                scripts.SCRIPT_PRIFILE_CHANNELS,
+                {},
+                this.targetDid,
+                this.context.getAppDid()
+            )
+            logger.debug(`Call script to query owned channels: ${result}`);
+
+            let items = result.find_message.items
             let channelInfos = []
-            result.forEach(item => {
-                const channelInfo = ChannelInfo.parse(this.targetDid, item)
-                channelInfos.push(channelInfo)
+            items.forEach((item: any) => {
+                channelInfos.push(ChannelInfo.parse(this.targetDid, item))
             })
-            logger.debug("query owned channels 'ChannelInfo': ", channelInfos)
+
+            logger.debug(`Got owned channels: ${channelInfos}`);
             return channelInfos
         } catch (error) {
             logger.error('query owned channels error: ', error)
@@ -89,16 +98,19 @@ export class Profile implements ProfileHandler {
     */
     public async queryOwnedChannnelById(channelId: string): Promise<ChannelInfo> {
         try {
-            const filter = {
-                "channel_id": channelId,
-            }
-            logger.debug("query owned channel by id params: ", filter)
-            const results = await this.vault.callScript(scripts.SCRIPT_QUERY_CHANNEL_INFO, filter, this.targetDid, this.context.getAppDid())
-            logger.debug("query owned channel by id success: ", results)
-            const result = results.find_message.items
-            const channelInfo = ChannelInfo.parse(this.targetDid, result[0])
-            logger.debug("query owned channel by id 'ChannelInfo': ", channelInfo)
-            return channelInfo
+            const result = await this.vault.callScript(
+                scripts.SCRIPT_QUERY_CHANNEL_INFO,
+                {"channel_id": channelId,},
+                this.targetDid,
+                this.context.getAppDid()
+            )
+            logger.debug(`Call script to query owned channel by id: ${result}`)
+
+            const items = result.find_message.items
+            let channelInfo =  ChannelInfo.parse(this.targetDid, items[0])
+
+            logger.debug(`Got owned channel by Id: ${channelInfo}`);
+            return channelInfo;
         } catch (error) {
             logger.error("query owned channel by id error: ", error)
             throw new Error(error)
@@ -106,15 +118,21 @@ export class Profile implements ProfileHandler {
     }
 
     /**
-     * //新增
-    * Query the number of channels subscribed by this profile
+     * Query the number of channels subscribed by this profile
     */
     public async querySubscriptionCount(): Promise<number> {
         try {
-            const filter = {}
-            const result = await this.vault.callScript(scripts.SCRIPT_PRIFILE_SUBSCRIPTIONS, filter, this.targetDid, this.context.getAppDid())
-            logger.debug("query subscription Count success: ", result)
-            return result.find_message.total
+            let result = await this.vault.callScript(
+                scripts.SCRIPT_PRIFILE_SUBSCRIPTIONS,
+                {},
+                this.targetDid,
+                this.context.getAppDid()
+            )
+            logger.debug(`Call script to subscription count: ${result}`)
+
+            let count = result.find_message.total;
+            logger.debug(`Got subscription count: ${count}`)
+            return count
         } catch (error) {
             logger.error("query subscription Count error: ", error)
             throw new Error(error)
@@ -122,29 +140,36 @@ export class Profile implements ProfileHandler {
     }
 
     /**
-    * 新增
     * Query the channels subscribed to by this profile
     */
     public async querySubscriptions(): Promise<ChannelInfo[]> {
         try {
-            const filter = {}
-            const results = await this.vault.callScript(scripts.SCRIPT_PRIFILE_SUBSCRIPTIONS, filter, this.targetDid, this.context.getAppDid())
-            logger.debug("query subscriptions success: ", results)
-            const result = results.find_message.items
+            const result = await this.vault.callScript(
+                scripts.SCRIPT_PRIFILE_SUBSCRIPTIONS,
+                {},
+                this.targetDid,
+                this.context.getAppDid()
+            )
+            logger.debug(`Call script to query subscriptions: ${result}`)
+
+            const items = result.find_message.items
             let subscriptions = []
-            for (let index = 0; index < result.length; index++) {
-                const item = result[index]
+
+            for (let index = 0; index < items.length; index++) {
+                const item = items[index]
                 const channel_id = item.channel_id
                 const target_did = item.target_did.toString()
-                const params = {
-                    "channel_id": channel_id,
-                }
-                const callScriptResult = await this.vault.callScript(scripts.SCRIPT_QUERY_CHANNEL_INFO, params, target_did, this.context.getAppDid())
-                const channelInfo = ChannelInfo.parse(target_did, callScriptResult.find_message.items[0])
-                subscriptions.push(channelInfo)
+
+                const info = await this.vault.callScript(
+                    scripts.SCRIPT_QUERY_CHANNEL_INFO,
+                    { "channel_id": channel_id },
+                    channel_id,
+                    this.context.getAppDid()
+                )
+                subscriptions.push(ChannelInfo.parse(target_did, info.find_message.items[0]))
             }
-            logger.debug("query subscriptions 'ChannelInfo': ", results)
-    
+
+            logger.debug(`Susbscriptions: ${subscriptions}`)
             return subscriptions
         } catch (error) {
             logger.error('query subscription channels error: ', error)
