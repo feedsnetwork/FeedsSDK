ad9782009d9ef05ecda0ecbc2a8fd3cb52ed3eaa
liaihong <>
2022-11-29 16:40:43 +0800

    #3wvh82u Refine code

--- src/index.ts
+++ src/index.ts
@@ -29,26 +29,3 @@ export {
     MyProfile,
     MyChannel,
 }
-/*
-interface ConfigOptions {
-    id: string;
-    url: string;
-}
-class LibraryStarter {
-    public id: string;
-
-    public url: string;
-
-    constructor(options: ConfigOptions) {
-        this.id = options.id;
-        this.url = options.url;
-    }
-
-    getConfig() {
-        return {
-            id: this.id,
-            url: this.url,
-        };
-    }
-}
-*/
